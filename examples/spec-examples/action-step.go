4a2cc43b9dbe183fcf265481cf020efab18af861
Bret Jordan <jordan2175@gmail.com>
2025-02-23 18:50:56 -0700

    more step examples

---
+++ examples/spec-examples/action-step.go
@@ -0,0 +1,42 @@
+// Copyright 2021 Bret Jordan, All rights reserved.
+//
+// Use of this source code is governed by an Apache 2.0 license that can be
+// found in the LICENSE file in the root of the source tree.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/openplaybooks/libcacao/objects/playbook"
+)
+
+func main() {
+	p := playbook.New()
+	p.Created = ""
+	p.Modified = ""
+
+	// Create workflow steps for this playbook
+	step1, _ := p.NewActionStep()
+	step1.Name = "Action Step 1"
+	step1.Description = "Example of a manual action step."
+
+	// Create workflow steps for this playbook
+	end, _ := p.NewEndStep()
+	end.Name = "Playbook End"
+	end.Description = "Example of an end step."
+
+	step1.OnSuccess = end.GetID()
+	step1.Agent = "individual--328a89ab-3b8f-40c4-a491-24a40bcd3cd4"
+	cmd1, _ := step1.NewCommand()
+	cmd1.ObjectType = "manual"
+	cmd1.Command = "Disconnect the infected machines from the network"
+
+	// Encode
+	data, err := p.EncodeToString()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(data)
+
+}
--- objects/workflow/setters.go
+++ objects/workflow/setters.go
@@ -13,10 +13,10 @@ import (
 // Define Action Step Functions and Methods
 // ----------------------------------------------------------------------
 
-// NewCommand - This method creates a new empty external reference and
-// returns a reference to it so it can be populated. However, if one or more
-// external references are passed in they are all added and the reference that
-// is returned is for the last entry added.
+// NewCommand - This method creates a new empty command and returns a reference
+// to it so it can be populated. However, if one or more commands are passed in
+// they are all added and the reference that is returned is for the last entry
+// added.
 func (s *ActionStep) NewCommand(r ...CommandData) (*CommandData, error) {
 	positionThatAppendWillUse := len(s.Commands)
 
@@ -33,6 +33,28 @@ func (s *ActionStep) NewCommand(r ...CommandData) (*CommandData, error) {
 	var c CommandData
 	s.Commands = append(s.Commands, c)
 	return &s.Commands[positionThatAppendWillUse], nil
+}
+
+// NewExternalReference - This method creates a new empty external reference and
+// returns a reference to it so it can be populated. However, if one or more
+// external references are passed in they are all added and the reference that
+// is returned is for the last entry added.
+func (s *ActionStep) NewExternalReference(r ...objects.ExternalReference) (*objects.ExternalReference, error) {
+	positionThatAppendWillUse := len(s.ExternalReferences)
+
+	if len(r) > 0 {
+		for i := range r {
+			// Update the value so we grab the last one entered
+			positionThatAppendWillUse = len(s.ExternalReferences)
+			s.ExternalReferences = append(s.ExternalReferences, r[i])
+		}
+		return &s.ExternalReferences[positionThatAppendWillUse], nil
+	}
+
+	// If one was not passed in, lets create one
+	var er objects.ExternalReference
+	s.ExternalReferences = append(s.ExternalReferences, er)
+	return &s.ExternalReferences[positionThatAppendWillUse], nil
 }
 
 // ----------------------------------------------------------------------
